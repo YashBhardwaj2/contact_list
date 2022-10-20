700f40cf40d58947c525deb387fdb41ccef4a0b5
YashBhardwaj2 <bhardwajyash136@gmail.com>
2022-10-20 19:34:40 +0530

    added assets middleware and parsing the contact list

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 const express = require('express');
 const path = require('path');
-const port =8000;
+const port = 8000;
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+// app.use(express.static('assets'));
+
+var contactList = [
+    {
+        name: 'Yash Bhardwaj',
+        phone: "7011808879"
+    },
+    {
+        name: "Pallav jain",
+        phone: "9873013365"
+    },
+    {
+        name: "Papa",
+        phone: "8860406090"
+    }
+];
+
+app.get('/',function(req, res){
+    return res.render('home',{
+        title:"I am Flying",
+        contact_list: contactList
+    });
+});
+
+app.get('/playground',function(req,res){
+    return res.render('playground');
+});
+
+app.post('/create_contact',function(req,res){
+    contactList.push(req.body);
+    return res.redirect('back');
+});
+
+app.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(`Server is up and running at port no ${port}`);
+    return;
+});
